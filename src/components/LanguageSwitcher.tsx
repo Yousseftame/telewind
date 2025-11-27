2adafa4e6c24f34fa615b43dc9c64443f1bd241d
Yousseftame <tamerosama73@gmail.com>
2025-11-27 12:49:16 +0200

    localization fr/de

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -17,6 +17,9 @@ import { cn } from "@/lib/utils";
 const languages = [
   { code: "en", name: "English", flag: "🇬🇧", apiCode: "en" },
   { code: "ar", name: "العربية", flag: "🇸🇦", dir: "rtl", apiCode: "ar" },
+  { code: "fr-FR", name: "Français", flag: "🇫🇷", apiCode: "fr" },
+{ code: "de-DE", name: "Deutsch", flag: "🇩🇪", apiCode: "de" }
+
   // { code: "zh-TW", name: "繁體中文", flag: "🇹🇼", apiCode: "tw" },
   // { code: "zh-CN", name: "简体中文", flag: "🇨🇳", apiCode: "ch" },
 ];
